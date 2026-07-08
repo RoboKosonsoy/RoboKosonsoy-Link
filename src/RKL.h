a88e062f79a9d3ea55a0eb6f172131480315f99f
DAVRON <investnet97@gmail.com>
2026-07-08 20:27:14 +0500

    feat: add main library header

---
+++ src/RKL.h
@@ -0,0 +1,25 @@
+#ifndef RKL_H
+#define RKL_H
+
+/*
+ * RoboKosonsoy Link (RKL)
+ * Main library header
+ *
+ * Copyright (c) RoboKosonsoy Community
+ * Licensed under the MIT License
+ */
+
+#include <Arduino.h>
+
+// Core
+#include "core/Types.h"
+#include "core/Config.h"
+#include "core/CRC16.h"
+
+// Drivers
+#include "drivers/E32.h"
+
+// Protocol
+#include "protocol/Link.h"
+
+#endif // RKL_H
